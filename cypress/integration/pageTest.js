b112c43fce702424f2a9d4f9ded204b5a004e094
Elias Herranen <elias.herranen@gmail.com>
2021-12-27 17:38:41 +0200

    fixing deployemtn

--- cypress/integration/pageTest.js
+++ cypress/integration/pageTest.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-undef */
 describe('Phonebook app', function() {
-    it('front page can be opened', function() {
-      cy.visit('http://localhost:3001')
-      cy.contains('Phonebook')
-      cy.contains('Add a new')
-    })
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:3001')
+    cy.contains('Phonebook')
+    cy.contains('Add a new')
   })
+})
